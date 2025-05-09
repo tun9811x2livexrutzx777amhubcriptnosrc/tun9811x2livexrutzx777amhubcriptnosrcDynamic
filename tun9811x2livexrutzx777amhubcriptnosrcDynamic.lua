19575b0a07cb7d992381760920307d0adf920993
tun9811x2livexrutzx777amhubcriptnosrc <savescripthub@gmail.com>
2025-05-09 18:41:22 +0700

    Update tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua

--- tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua
+++ tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua
@@ -1,45 +1,35 @@
-
-local mapIDs = {2753915549, 4442272183, 7449423635, 16732694052, 18668065416, 99999183305180, 92517437168342,87039211657390,128336380114944,72992062125248}
-local function loadScript(url)
-    local success, result = pcall(function()
-        return loadstring(game:HttpGet(url))()
-    end)
-    if not success then
-        warn("ไม่สามารถโหลดสคริปต์ได้:", result)
-        game.Players.LocalPlayer:Kick("Haven't made a script for this map yet. The creator is still fat.")
-    end
-end
-if table.find(mapIDs, game.PlaceId) then
-    if getgenv().Mode == "Kaitun" then
-        if game.PlaceId == 18668065416 or game.PlaceId == 99999183305180 then
-            game.Players.LocalPlayer:Kick("Haven't done it yet.")
+local Executor = {
+    -- PC
+    "Wave",
+    "AWP",
+    "Synapse Z",
+    "Velocity",
+    "Seliware",
+    "Swift",
+    -- Mobile
+    "Delta",
+    "Arceus X",
+    "Codex",
+    "Cryptic",
+    "Krnl",
+}
+local Game = {
+    [2753915549] = "https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicBF/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua",
+    [4442272183] = "https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicBF/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua",
+    [7449423635] = "https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicBF/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua",
+}
+for i,v in pairs(Executor) do
+    local e = identifyexecutor()
+    if e == v then
+        for i,v in pairs(Game) do
+            local p = game.PlaceId
+            if p == i then
+                loadstring(game:HttpGet(v))()
+            else
+                game.Players.LocalPlayer:Kick("ยังไม่ได้ทำ")
+            end
         end
     else
-        -- โหลดสคริปต์ตาม ID ของเกม
-        if game.PlaceId == 2753915549 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicBF/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua"))()
-        elseif game.PlaceId == 4442272183 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicBF/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua"))()
-        elseif game.PlaceId == 7449423635 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicBF/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua"))()
-        --[[
-        if game.PlaceId == 16732694052 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/tun9811/testtttttttttttttttfisch/refs/heads/main/script.lua"))()
-        elseif game.PlaceId == 18668065416 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicBLR/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua"))()
-        elseif game.PlaceId == 99999183305180 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicBLR/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua"))()
-        elseif game.PlaceId == 92517437168342 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicBLR/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua"))()
-        elseif game.PlaceId == 87039211657390 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicAC/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua"))()
-        elseif game.PlaceId == 128336380114944 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicAC/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua"))()
-        elseif game.PlaceId == 72992062125248 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicH/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua"))()
-                --]]
-        end
+        game.Players.LocalPlayer:Kick("Script does not support " .. identifyexecutor())
     end
-else
-    game.Players.LocalPlayer:Kick("ยังไม่ได้ทำ")
 end
