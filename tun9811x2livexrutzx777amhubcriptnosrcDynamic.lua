abe23989bf235abbf20bfd228151f39d26f7f83b
tun9811x2livexrutzx777amhubcriptnosrc <savescripthub@gmail.com>
2025-05-15 20:08:56 +0700

    Update tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua

--- tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua
+++ tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua
@@ -13,30 +13,25 @@ local Executor = {
     "Cryptic",
     "Krnl",
 }
+
 local Game = {
     [2753915549] = "https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicBF/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua",
     [4442272183] = "https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicBF/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua",
     [7449423635] = "https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicBF/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua",
-    [10449761463] = "https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicTHB/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua"
+    [10449761463] = "https://raw.githubusercontent.com/tun9811x2livexrutzx777amhubcriptnosrc/tun9811x2livexrutzx777amhubcriptnosrcDynamicTHB/refs/heads/main/tun9811x2livexrutzx777amhubcriptnosrcDynamic.lua",
 }
-local c = 0
-for i,v in pairs(Executor) do
-    local e = identifyexecutor()
-    if table.find(Executor, e) then
-        for i,v in pairs(Game) do
-            local p = game.PlaceId
-            if p == i then
-                if c == 0 then
-                    loadstring(game:HttpGet(v))()
-                    c += 1
-                else
-                    break
-                end
-            else
-                game.Players.LocalPlayer:Kick("ยังไม่ได้ทำ")
-            end
-        end
-    else
-        game.Players.LocalPlayer:Kick("Script does not support " .. identifyexecutor())
-    end
+
+local executorName = identifyexecutor()
+local placeId = game.PlaceId
+
+if not table.find(Executor, executorName) then
+    game.Players.LocalPlayer:Kick("Script does not support " .. executorName)
+    return
+end
+
+local url = Game[placeId]
+if url then
+    loadstring(game:HttpGet(url))()
+else
+    game.Players.LocalPlayer:Kick("ยังไม่ได้ทำ")
 end
